a26e4be86082cd66b4ba79e9159e10afa74e91df
Munchic <khoi.u.pham@gmail.com>
2020-03-05 01:22:33 -0300

    package architecture

---
+++ impepdom/__init__.py
@@ -0,0 +1,4 @@
+from .mlp import MultilayerPerceptron
+from .dataloader import PeptideDataset
+
+__all__ = ['MultilayerPerceptron', 'PeptideDataset']
--- impepdom/mlp.py
+++ impepdom/mlp.py
@@ -4,11 +4,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-class Net(nn.Module):
+class MultilayerPerceptron(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(MultilayerPerceptron, self).__init__()
         self.fc1 = nn.Linear(1,2)
         self.fc2 = nn.Linear(2,1)
+        
     def forward(self, x):
         x = self.fc2(F.relu(self.fc1(x)))
         return x
