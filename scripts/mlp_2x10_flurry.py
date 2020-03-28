3a0198855b5a614562e1a43f1f7b4ebd4b988de1
Munchic <khoi.u.pham@gmail.com>
2020-03-28 11:53:25 -0300

    distributed hyperparam-search for alleles

--- scripts/mlp_2x10_flurry.py
+++ scripts/mlp_2x10_flurry.py
@@ -4,14 +4,30 @@ sys.path.append("..")  # add top folder to path
 
 import impepdom
 
-hla_alleles = os.listdir('../datasets/MHC_I_el_allele_specific')
+# os.listdir('../datasets/MHC_I_el_allele_specific')
+hla_alleles = [
+    'HLA-B44:03',  # >>> Khoi 1
+    'HLA-B08:01',
+    'HLA-A01:01',
+    'HLA-B15:01',  # >>> Khoi 1
+    'HLA-A02:01',  # >>> Khoi 2
+    'HLA-A03:01',  
+    'HLA-B07:02',  # <<< Khoi 2
+    'HLA-A24:02',  # >>> Michael
+    'HLA-B27:05', 
+    'HLA-A68:01'   # <<< Michael
+]
+
+hla_alleles_khoi_1 = hla_alleles[:4]
+hla_alleles_khoi_2 = hla_alleles[4:7]
+hla_alleles_michael = hla_alleles[7:]
 
 model = impepdom.models.MultilayerPerceptron(num_hidden_layers=2, hidden_layer_size=100)
 
 for i, hla_allele in enumerate(hla_alleles):
     print('working with allele {0} out of {1}'.format(i + 1, len(hla_alleles)))
     dataset = impepdom.PeptideDataset(
-        hla_allele=hla_allele,
+        hla_allele=hla_allele,  # change allele here
         padding='flurry',
         toy=False)
         
