bcca737263f789e86cb0a60d8c57912826bb5851
Munchic <khoi.u.pham@gmail.com>
2020-03-28 12:21:14 -0300

    passed a list instead of single allele

--- scripts/mlp_2x10_flurry.py
+++ scripts/mlp_2x10_flurry.py
@@ -24,19 +24,19 @@ hla_alleles_michael = hla_alleles[7:]
 
 model = impepdom.models.MultilayerPerceptron(num_hidden_layers=2, hidden_layer_size=100)
 
-for i, hla_allele in enumerate(hla_alleles):
-    print('working with allele {0} out of {1}'.format(i + 1, len(hla_alleles)))
+for i, hla_allele in enumerate(hla_alleles_michael):  # change allele list here
+    print('working with allele {0} out of {1}'.format(i + 1, len(hla_alleles_michael)))  # change allele list here
     dataset = impepdom.PeptideDataset(
-        hla_allele=hla_alleles_michael,  # change allele here
+        hla_allele=hla_allele,  
         padding='flurry',
         toy=False)
         
     results = impepdom.hyperparam_grid_search(
         model,
         dataset,
-        epochs=[5, 10, 15],
-        batch_sizes=[32, 64, 128],
-        learning_rates=[5e-4, 1e-3, 5e-3, 1e-2, 5e-2],
+        epochs=[1] # [5, 10, 15],
+        batch_sizes=[32]  # [32, 64, 128],
+        learning_rates=[5e-3] # [5e-4, 1e-3, 5e-3, 1e-2, 5e-2],
     )
 
     for res in results:
